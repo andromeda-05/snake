3fc7ab031f2c221cb7ea2f51fe50219c25a22149
Valery <andromeda-05@yandex.ru>
2021-12-31 18:31:00 +0400

    first fix

---
+++ snake.py
@@ -0,0 +1,36 @@
+import pygame
+import time
+
+pygame.init()
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+
+WIDTH = 500
+HEIGHT = 500
+
+screen = pygame.display.setmode((WIDTH,HEIGHT))
+
+pygame.display.set_caption('ZMEYKA')
+
+cur_x = WIDTH/2
+cur_y = HEIGHT/2
+
+dx = 0
+dy = 0
+
+delta = 10
+
+flag_stop = False
+
+clock = pygame.time.clock()
+
+while not flag_stop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUITE:
+            flag_stop = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_LEFT:
+                
+
